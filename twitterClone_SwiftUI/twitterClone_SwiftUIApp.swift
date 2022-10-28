befacaf0c756c321f9643f5577a8955f11c897ce
paxmiler <paxmiler@gmail.com>
2022-10-28 15:11:08 +0900

    Initial Commit

---
+++ twitterClone_SwiftUI/twitterClone_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  twitterClone_SwiftUIApp.swift
+//  twitterClone_SwiftUI
+//
+//  Created by Martin.Q on 2022/10/28.
+//
+
+import SwiftUI
+
+@main
+struct twitterClone_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
