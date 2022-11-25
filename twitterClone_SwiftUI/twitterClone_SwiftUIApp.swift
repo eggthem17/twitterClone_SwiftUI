db6082d0726d3aabccf2cf9d3013adc298d7e15e
paxmiler <paxmiler@gmail.com>
2022-11-25 11:31:59 +0900

    + import Firebase

--- twitterClone_SwiftUI/twitterClone_SwiftUIApp.swift
+++ twitterClone_SwiftUI/twitterClone_SwiftUIApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct twitterClone_SwiftUIApp: App {
+	init() {
+		FirebaseApp.configure()
+	}
+	
     var body: some Scene {
         WindowGroup {
 			NavigationStack {
