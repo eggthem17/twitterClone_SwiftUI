3aca125c318ea73d9344df3c6a4e9bbb93ba728f
paxmiler <paxmiler@gmail.com>
2022-12-05 17:46:47 +0900

    * code refactoring on UserServuce/fetchUsers

--- twitterClone_SwiftUI/Service/UserService.swift
+++ twitterClone_SwiftUI/Service/UserService.swift
@@ -24,20 +24,22 @@ struct UserService {
 	}
 	
 	func fetchUsers(completion: @escaping([User]) -> Void) {
-		var users = [User]()
+//		var users = [User]()
 		
 		Firestore.firestore().collection("users").getDocuments { snapshot, _ in
 			guard let documents = snapshot?.documents else {
 				return
 			}
 			
-			documents.forEach { document in
-				guard let user = try? document.data(as: User.self) else {
-					return
-				}
-				
-				users.append(user)
-			}
+			let users = documents.compactMap({ try? $0.data(as: User.self) })
+			
+//			documents.forEach { document in
+//				guard let user = try? document.data(as: User.self) else {
+//					return
+//				}
+//
+//				users.append(user)
+//			}
 			
 			completion(users)
 		}
